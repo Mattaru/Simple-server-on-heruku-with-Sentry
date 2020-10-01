a920bda7e0605a4e05613af76a1bc2c136cade83
Mattaru <67161027+Mattaru@users.noreply.github.com>
2020-10-01 12:22:37 +0300

    22

--- HWserver.py
+++ HWserver.py
@@ -25,7 +25,7 @@ def get_error():
 if os.environ.get('SERVER_URL') == 'https://fathomless-escarpment-57347.herokuapp.com':
     run(
         host='0.0.0.0',
-        port=int(os.environ.get('port', 5000)),
+        port=int(os.environ.get('PORT', 5000)),
         server='gunicorn',
         workers=3
     )
