577b5a8f56f53899fc425fd8061dea746a88830a
Mattaru <67161027+Mattaru@users.noreply.github.com>
2020-10-01 12:42:13 +0300

    rrr

--- HWserver.py
+++ HWserver.py
@@ -1,26 +1,24 @@
 import os
 import sentry_sdk
-from bottle import Bottle, run
+from bottle import run, route
 from sentry_sdk.integrations.bottle import BottleIntegration
 
 sentry_sdk.init(
     dsn='https://1543850c449c47cc9728989f67816ef2@o453036.ingest.sentry.io/5441339',
     integrations=[BottleIntegration()]
 )
-app = Bottle()
 
-@app.route('/')
+@route('/')
 def im_ok():
     return 'Im ok.'
 
-@app.route('/success')
+@route('/success')
 def get_success():
     return 'I am ready for work.'
 
-@app.route('/fail')
+@route('/fail')
 def get_error():
     raise RuntimeError('Server error for the test')
-    return
 
 if os.environ.get('SERVER_URL') == 'https://fathomless-escarpment-57347.herokuapp.com':
     run(
